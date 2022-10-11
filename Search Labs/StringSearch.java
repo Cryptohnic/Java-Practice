7697dab29522a3aae27e901fd92d61df38859b97
Brandon A <brandonwalex@gmail.com>
2022-10-10 22:10:51 -0500

    Update StringSearch.java

--- Search Labs/StringSearch.java
+++ Search Labs/StringSearch.java
@@ -2,7 +2,7 @@
 //www.apluscompsci.com
 
 public class StringSearch{
-	public static int countWords(String sent, String word){
+	public static int countWords(String sent,String word){
 		int count=0;
 		for(int i=0;i<sent.length()-word.length()+1;i++){
 			if(sent.substring(i,i+word.length()).equals(word))
@@ -11,7 +11,7 @@ public class StringSearch{
 		return count;
 	}
 
-	public static int countLetters(String sent, String letter)
+	public static int countLetters(String sent,String letter)
 	{
 		int count=0;
 		for(char c:sent.toCharArray()){
