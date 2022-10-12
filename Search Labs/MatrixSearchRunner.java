855b790da36bd975cc6bb68b34b4fa7f2fcea4b5
Brandon A <brandonwalex@gmail.com>
2022-10-12 12:34:28 -0500

    New Files

---
+++ Search Labs/MatrixSearchRunner.java
@@ -0,0 +1,16 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - 
+
+import java.util.*;
+import java.io.*; 
+
+public class MatrixSearchRunner
+{
+    public static void main(String[] args) throws Exception
+    {
+    	  MatrixSearch ms = new MatrixSearch(40, 40, 15);
+        System.out.println( "Odd count = " + ms.countOdds() );
+        //call count evens
+    }
+}
