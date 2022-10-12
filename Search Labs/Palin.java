5e53450338fd5e25fe3a2ec36a886dc40910f405
Brandon A <brandonwalex@gmail.com>
2022-10-12 09:03:34 -0500

    Finished Labs

--- Search Labs/Palin.java
+++ Search Labs/Palin.java
@@ -1,28 +1,39 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date - 
-//Class -
-//Lab  -
-
-import static java.lang.System.*;
-import java.util.ArrayList;
+//Â© A+ Computer Science  -  www.apluscompsci.com
 
 //define class Palin
-
-
+public class Palin{
 	//instance variable - String
-
-
+	String s;
 	//constructors
+	public Palin(){
+		s="";
+	}
 
-
+	public Palin(String word){
+		s=word;
+	}
 	//getLength method - returns an int
-
+	public int getLength(){
+		return s.length();
+	}
 
 	//getWord method - returns a String
-
+	public String getWord(){
+		return s;
+	}
 
 	//isPalin method - returns a boolean
-
+	public boolean isPalin(){
+		String reverse="";
+		for(char c:s.toCharArray())
+			reverse=c+reverse;
+		return s.equals(reverse);
+	}
 
 	//toString method - returns a String
+	public String toString(){
+		return s;
+	}
+}
+
+	
--- Search Labs/PalinRunner.java
+++ Search Labs/PalinRunner.java
@@ -1,22 +1,13 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name - 
-//Date -
-//Class -
-//Lab  -
+//Â© A+ Computer Science  -  www.apluscompsci.com
 
-import java.util.Collections;
-import java.util.ArrayList;
 import static java.lang.System.*;
 
-public class PalinRunner
-{
-	public static void main(String args[])
-	{
-		String[] words = "dog dad racecar goofy madam alligator benjamin".split(" ");
-		for( String s : words )
-		{
-			Palin one = new Palin( s );
-		   System.out.print(one.isPalin() ? one + " is a palindrome.\n" : one + " is not a palindrome.\n");
+public class PalinRunner{
+	public static void main(String args[]){
+		String[] words="dog dad racecar goofy madam alligator benjamin".split(" ");
+		for(String s:words){
+			Palin one = new Palin(s);
+			out.print(one.isPalin()?one+" is a palindrome.\n":one+" is not a palindrome.\n");
 		}
 	}
 }
