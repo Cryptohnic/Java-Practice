e9961414db40020ab698aa8d2f4cfd73ccd86491
Brandon A <brandonwalex@gmail.com>
2022-10-13 04:33:42 -0500

    More Labs Complete

--- Search Labs/MatrixSearch.java
+++ Search Labs/MatrixSearch.java
@@ -1,9 +1,5 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name - 
-
-import java.util.*;
-import java.io.*; 
 
 public class MatrixSearch
 {
@@ -14,41 +10,53 @@ public class MatrixSearch
 		 *post - mat will be rows X cols
 		 *post - mat will contain integers that are <= upper and >= 1
 		 */
-    public MatrixSearch( int rows, int cols, int upper )
-    {
+    public MatrixSearch(int rows,int cols,int upper){
+		mat=new int[rows][cols];
+		for(int r=0;r<rows;r++)
+			for(int c=0;c<cols;c++)
+				mat[r][c]=(int)(Math.random()*upper)+1;
     }
-
 
 		/*
 		 *pre - mat is not null
 		 *post - count of odd numbers will be returned
 		 *post - no values in mat will have been changed
 		 */
-    public int countOdds( )
-    {
-		return 0;
+    public int countOdds(){
+		int odds=0;
+		for(int r=0;r<mat.length;r++)
+			for(int c=0;c<mat[r].length;c++)
+				if(mat[r][c]%2==1)
+					odds++;
+		return odds;
     }
-
 
 		/*
 		 *pre - mat is not null
 		 *post - count of even numbers will be returned
 		 *post - no values in mat will have been changed
 		 */
-    public int countEvens( )
-    {
-		return 0;
+    public int countEvens(){
+		int evens=0;
+		for(int r=0;r<mat.length;r++)
+			for(int c=0;c<mat[r].length;c++)
+				if(mat[r][c]%2==0)
+					evens++;
+		return evens;
     }
-
 
 		/*
 		 *pre - mat is not null
 		 *post - count of prime numbers will be returned
 		 *post - no values in mat will have been changed
 		 */
-    public int countPrimes( )
-    {
-		return 0;
+    public int countPrimes(){
+		int primes=0;
+		for(int r=0;r<mat.length;r++)
+			for(int c=0;c<mat[r].length;c++)
+				if(isPrime(mat[r][c]))
+					primes++;
+		return primes;
     }
 
 		/*
@@ -56,17 +64,18 @@ public class MatrixSearch
 		 *post - false is returned if num is divisble by any number between 2 and itself
 		 *post - true is returned if num is not divisble by any number between 2 and itself
 		 */
-    private boolean isPrime( int num )
-    {
-    	return false;
+    private boolean isPrime(int num){
+		for(int i=2;i<num;i++)
+			if(num%i==0)
+				return false;
+    	return true;
     }
 
 		/*
 		 *pre - mat is not null
 		 *post - all values from mat are concatenated to a string and returned
 		 */
-    public String toString()
-    {
- 		return "";
+    public String toString(){
+		
     }
 }
