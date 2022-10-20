5552106806d80839fe65d8f3af58171538fb9fa8
Brandon A <brandonwalex@gmail.com>
2022-10-19 21:12:26 -0500

    Update MatrixSearchRunner.java

--- Search Labs/MatrixSearchRunner.java
+++ Search Labs/MatrixSearchRunner.java
@@ -5,19 +5,19 @@ import static java.lang.System.out;
 
 public class MatrixSearchRunner{
     public static void main(String[] args){
-    	MatrixSearch ms = new MatrixSearch(20,20,25);
+    	MatrixSearch ms=new MatrixSearch(20,20,25);
         out.println(ms);
         out.println("Odd count = "+ms.countOdds());
         out.println("Even count = "+ms.countEvens());
         out.println("Prime count = "+ms.countPrimes());
         out.println("\n\n");
-        ms = new MatrixSearch(10,10,50);
+        ms=new MatrixSearch(10,10,50);
         out.println(ms);
         out.println("Odd count = "+ms.countOdds());
         out.println("Even count = "+ms.countEvens());
         out.println("Prime count = "+ms.countPrimes());
         out.println("\n\n");
-        ms = new MatrixSearch(7,7,100);
+        ms=new MatrixSearch(7,7,100);
         out.println(ms);
         out.println("Odd count = "+ms.countOdds());
         out.println("Even count = "+ms.countEvens());
