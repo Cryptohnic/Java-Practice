f3ec3922ea3db77e27b9936d7b11c5fb6dca9e51
Brandon A <brandonwalex@gmail.com>
2022-10-28 09:22:05 -0500

    Merge Sort Labs

--- Sort Labs/MergeSort.java
+++ Sort Labs/MergeSort.java
@@ -1,57 +1,40 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date - 
-//Class - 
-//Lab  - 
+//(c) A+ Computer Science  -  www.apluscompsci.com
 
-import static java.lang.System.*;
+import static java.lang.System.out;
 import java.util.Arrays;		//use Arrays.toString() to help print out the array
 
-public class MergeSort
-{
+public class MergeSort{
 	private static int passCount;
 
-	public static void mergeSort(Comparable[] list)
-	{
-
-
+	public static void mergeSort(Comparable[] list){
 		passCount=0;
-		mergeSort(list, 0, list.length);
+		mergeSort(list,0,list.length);
 	}
 
-	private static void mergeSort( Comparable[] list, int front, int back)  //O( Log N )
-	{
-
-
-
-
-
-
-
+	private static void mergeSort(Comparable[] list,int front,int back){  //O(log(N))
+		int mid=(front+back)/2;
+		if(mid==front) return;
+		mergeSort(list,front,mid);
+		mergeSort(list,mid,back);
+		merge(list,front,back);
+		out.println("pass "+passCount+" "+Arrays.toString(list));
+		passCount++;
 	}
 
-	private static void merge(Comparable[] list, int front, int back)  //O(N)
-	{
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	private static void merge(Comparable[] list, int front, int back){  //O(N)
+		Comparable<?>[] temp=new Comparable[back-front];
+		int i=front, j=(front+back)/2, k=0, mid=j;
+		while(i<mid && j<back){
+			if(list[i].compareTo(list[j])<0)
+				temp[k++]=list[i++];
+			else
+				temp[k++]=list[j++];
+		}
+		while(i<mid)
+			temp[k++]=list[i++];
+		while(j<back)
+			temp[k++]=list[j++];
+		for(int p=0;p<temp.length;p++)
+			list[front+p]=temp[p];
 	}
 }
--- Sort Labs/MergeSortRunner.java
+++ Sort Labs/MergeSortRunner.java
@@ -1,19 +1,11 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  - 
-
-public class MergeSortRunner
-{
-	public static void main( String args[] )
-	{
+//(c) A+ Computer Science  -  www.apluscompsci.com
+import static java.lang.System.out;
+public class MergeSortRunner{
+	public static void main( String args[] ){
 		MergeSort.mergeSort(new Comparable[]{9,5,3,2});
-
-
+		out.println("\n\n");
 		MergeSort.mergeSort(new Comparable[]{19,52,3,2,7,21});
-
-
-		MergeSort.mergeSort(new Comparable[]{68,66,11,2,42,31});
+		out.println("\n\n");
+		MergeSort.mergeSort(new Comparable[]{68, 66,11,2,42,31});
 	}
 }
