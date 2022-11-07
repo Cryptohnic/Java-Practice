de240c41260cf23a08175fca19866952146b326c
Brandon A <brandonwalex@gmail.com>
2022-11-07 12:39:00 -0600

    Sets Labs

---
+++ Sets Labs/OddEvenSets.java
@@ -0,0 +1,32 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Scanner;
+
+public class OddEvenSets{
+	private Set<Integer> odds;
+	private Set<Integer> evens;
+
+	public OddEvenSets(){
+		odds=new TreeSet<>();
+		evens=new TreeSet<>();
+	}
+
+	public OddEvenSets(String line){
+		this();
+		Scanner s=new Scanner(line);
+		while(s.hasNext()){
+			int num=s.nextInt();
+			if(num%2!=0)
+				odds.add(num);
+			else
+				evens.add(num);
+		}
+	}
+
+	public String toString(){
+		return "ODDS : " + odds + "\nEVENS : " + evens + "\n\n";
+	}
+}
