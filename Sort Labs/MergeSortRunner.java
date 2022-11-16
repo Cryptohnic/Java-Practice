f01eafe0d20e7d38ba0fc9e28ab7cd07a0bb1a31
Brandon A <brandonwalex@gmail.com>
2022-11-16 09:54:27 -0600

    Small Tweaks

--- Sort Labs/MergeSortRunner.java
+++ Sort Labs/MergeSortRunner.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science  -  www.apluscompsci.com
 import static java.lang.System.out;
 public class MergeSortRunner{
-	public static void main( String args[] ){
+	public static void main(String args[]){
 		MergeSort.mergeSort(new Comparable[]{9,5,3,2});
 		out.println("\n\n");
 		MergeSort.mergeSort(new Comparable[]{19,52,3,2,7,21});
