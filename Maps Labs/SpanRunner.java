2154fb832926d9728853ab4ba32955b96954e2ed
Brandon A <brandonwalex@gmail.com>
2023-05-24 12:05:05 -0500

    Updates

---
+++ Maps Labs/SpanRunner.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class SpanRunner
+{
+	public static void main( String args[] ) throws IOException
+	{
+		SpanishToEnglish spanishToEnglish=new SpanishToEnglish();
+		Scanner s=new Scanner(new File("spantoeng.dat"));
+		int count=s.nextInt();
+		s.nextLine();
+		for(int i=0;i<count;i++){
+			spanishToEnglish.putEntry(s.nextLine());
+		}
+		out.println(spanishToEnglish);
+		while(s.hasNextLine())
+			out.println(spanishToEnglish.translate(s.nextLine()));
+	}
+}
---
+++ Maps Labs/SpanishToEnglish.java
@@ -0,0 +1,51 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class SpanishToEnglish
+{
+	private Map<String,String> pairs;
+
+	public SpanishToEnglish()
+	{
+		pairs=new TreeMap<>();
+	}
+
+	public void putEntry(String entry)
+	{
+		String[] text=entry.split(" ");
+		pairs.put(text[0],text[1]);
+	}
+
+	public String translate(String sent)
+	{
+		Scanner chop = new Scanner(sent);
+		String output="";
+		while(chop.hasNext()){
+			String word=chop.next();
+			String noPunct=word.replaceAll("\\p{Punct}","");
+			if(pairs.get(noPunct)!=null){
+				if(word==noPunct)
+					output+=pairs.get(word)+" ";
+				else
+					output+=pairs.get(noPunct)+". ";
+					
+			}
+			else
+				output+=word+" ";
+		}
+		chop.close();
+		return output;
+	}
+
+	public String toString()
+	{
+		return "====\tMAP CONTENTS\t====\n\n"+pairs.toString()+"\n\n";
+	}
+}
