888a546af236d08456f39b2e451b50cbb492d4a6
Brandon A <brandonwalex@gmail.com>
2023-05-25 11:15:10 -0500

    Updates

---
+++ Number Systems/Number.java
@@ -0,0 +1,42 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+public class Number implements Comparable<Number>
+{
+
+	//add in instance variables
+	String number;
+	Integer base10;
+	String binary;
+	//add in a constructor or two
+
+	public Number(String num, int b){
+		number=num;
+		base10=Integer.parseInt(num,b);
+		binary=Integer.toString(base10,2);
+	}
+
+	@Override
+	public int compareTo(Number param)
+	{
+		int currOnes=binary.replaceAll("0","").length();
+		int paramOnes=param.binary.replaceAll("0","").length();
+		if(currOnes==paramOnes){
+			if(base10!=param.base10)
+				return base10.compareTo(param.base10);
+			return number.compareTo(param.number);
+		}
+		else if(currOnes>paramOnes)
+			return 1;
+		else
+			return -1;
+	}
+	
+	@Override
+	public String toString( )
+	{
+	  return number+" "+base10+" "+binary;
+	}
+}
---
+++ Number Systems/NumberRunner.java
@@ -0,0 +1,29 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+ 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Collections;
+import java.util.ArrayList;
+import static java.lang.System.*;
+
+public class NumberRunner
+{
+	public static void main ( String[] args ) throws IOException
+	{
+		Scanner s=new Scanner(new File("number.dat"));
+		int times=s.nextInt();
+		ArrayList<Number> numList=new ArrayList<>();
+		s.nextLine();
+		for(int i=0;i<times;i++){
+			String[] line=s.nextLine().split(" ");
+			numList.add(new Number(line[0],Integer.parseInt(line[1])));
+		}
+		Collections.sort(numList);
+		for(Number n : numList)
+			out.println(n);
+	}
+}
