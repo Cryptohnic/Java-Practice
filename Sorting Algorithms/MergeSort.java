a6cb724c15cbaba7f6c38e652b8c1ec48e55a2d3
Cryptohnic <brandonwalex@gmail.com>
2023-05-29 11:26:56 -0500

    Updates

--- Sorting Algorithms/MergeSort.java
+++ Sorting Algorithms/MergeSort.java
@@ -14,14 +14,14 @@ public class MergeSort{
 		int n=list.length;
 		for(;subSize<=n;subSize*=2){ // split the array into mini arrays of length 2
 			int left=0;
-			for(;left+subSize-1<n;left+=subSize){
+			for(;left+subSize-1<n;left+=subSize){ // while we can still make a subarray
 				int right=left+subSize-1;
 				int mid=(left+right)/2;
-				merge(list,left,mid,right); // sort the arrays at the middle
+				merge(list,left,mid,right); // sort the two smaller sorted arrays together
 			}
 			out.println("pass "+passCount+++" "+Arrays.toString(list));
 		}
-		if(subSize/2<n){ // if there are remaining values left on the end due to a list length not a multiple of 2, sort them 
+		if(subSize/2<n){ // if there are remaining values left on the end due to a list length not a multiple of 2, sort the entire sorted array with the remaining sorted edge values 
 			merge(list,0,subSize/2-1,n-1);
 			out.println("pass "+passCount+++" "+Arrays.toString(list));
 		}
