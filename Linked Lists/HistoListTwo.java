ddc9132d467116a2dd334d7c63d46588f021a49a
Cryptohnic <brandonwalex@gmail.com>
2023-06-01 16:56:34 -0500

    New Files

---
+++ Linked Lists/HistoListTwo.java
@@ -0,0 +1,61 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+public class HistoListTwo
+{
+	private ListNode front;
+
+	public HistoListTwo( )
+	{
+		front=null;
+	}
+
+
+	//ADDS NEW NODE TO THE FRONT OF THE LIST FOR LET IF IT DOES NOT EXIST.
+	//IF IT EXISTS, IT BUMPS UP LET'S COUNT BY ONE
+	public void add(Object obj)
+	{
+		int index=indexOf(obj);
+		if(index==-1)
+			front=new ListNode(new ThingCount(obj,1),front); // create a new ThingCount to put at the front for the new Object
+		else{
+			ListNode current=nodeAt(index); // get the already existing Node
+			((ThingCount)current.getValue()).setCount(((ThingCount)current.getValue()).getCount()+1); // update the count stored of the Object
+		}
+	}
+
+	//RETURNS THE INDEX POSITION OF LET IN THE LIST
+	public int indexOf(Object obj)
+	{
+		int spot=0;
+		ListNode current=front;
+		while(current!=null){
+			if(current.getValue().compareTo(new ThingCount(obj,0))==0) // if the object stored is the same
+				return spot;
+			current=current.getNext();
+			spot++;
+		}
+		return -1;
+	}
+
+	//RETURNS A REFERENCE TO THE NODE AT SPOT
+	private ListNode nodeAt(int spot)
+	{
+		ListNode current=front;
+		for(int i=0;i<spot;i++)
+			current=current.getNext();
+		return current;
+	}
+
+	//RETURNS THE LIST AS A BIG STRING
+	public String toString()
+	{
+		String output = "";
+		ListNode current=front;
+		while(current!=null){
+			output+=((ThingCount)current.getValue()).getThing()+" - "+((ThingCount)current.getValue()).getCount()+"\t";
+			current=current.getNext();
+		}
+		return output;
+	}
+}
---
+++ Linked Lists/HistogramListTwoRunner.java
@@ -0,0 +1,42 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+public class HistogramListTwoRunner
+{
+	public static void main(String[] args)
+	{
+		HistoListTwo test = new HistoListTwo();
+		String testString = "ABCDEFABCDEFFEDCBAAAAABBBBBCCCDAAAAAAAEEFFF";
+		for(char c : testString.toCharArray())
+		{
+			test.add(c);
+		}
+		System.out.println(test);
+
+
+		//add more test cases
+		test = new HistoListTwo();
+		testString = "11 22 33 44 55 66 33 44 22 11 11 11 11 22 11 11 11";
+		for(String c : testString.split(" "))
+		{
+			test.add(Integer.parseInt(c));
+		}
+		System.out.println(test);
+
+		test = new HistoListTwo();
+		testString = "1.1 2.2 3.3 4.4 5.5 6.6 3.3 4.4 2.2 1.1 1.1 1.1 1.1 2.2 1.1";
+		for(String c : testString.split(" "))
+		{
+			test.add(Double.parseDouble(c));
+		}
+		System.out.println(test);
+
+		
+		//demonstrate bad data check
+		test = new HistoListTwo();
+		test.add("dog");
+		test.add(33);
+		test.add(3.4);
+		System.out.println(test);
+	}
+}
