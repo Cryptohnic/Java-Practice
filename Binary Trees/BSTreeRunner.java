c3578cdd6e5da920c78c5d19d120836283a4b45c
Brandon A <brandonwalex@gmail.com>
2023-06-06 10:09:40 -0500

    Updates

--- Binary Trees/BSTreeRunner.java
+++ Binary Trees/BSTreeRunner.java
@@ -5,7 +5,7 @@ import static java.lang.System.out;
 
 public class BSTreeRunner
 {
-   public static void main( String args[] )
+   public static void main(String args[])
    {
  		BinarySearchTree bs=new BinarySearchTree();
       bs.add(90);
