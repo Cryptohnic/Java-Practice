0e07c0fcc6405ebd9a3d48bf2e23194414fda7a8
Brandon A <brandonwalex@gmail.com>
2023-08-19 16:43:58 -0500

    Updates

--- Graphs/ShortestPathGraph.java
+++ Graphs/ShortestPathGraph.java
@@ -1,36 +1,49 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-
-import java.io.File;
-import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.Scanner;
-import static java.lang.System.*;
 
 public class ShortestPathGraph
 {
-	private TreeMap<String, String> map;
+	private Map<String, String> map;
 	private boolean found;
 	private int shortest;
 
-	public ShortestPathGraph(String line)
-	{
+	public ShortestPathGraph(String line){
+		map=new TreeMap<>();
+		found=false;
+		String[] connections=line.split(" ");
+		for(String connection : connections){
+			String firstVal=connection.substring(0,1);
+			String secondVal=connection.substring(1);
+			if(map.get(firstVal)==null)
+				map.put(firstVal,"");
+			if(map.get(secondVal)==null)
+				map.put(secondVal,"");
+			map.put(firstVal,map.get(firstVal)+secondVal); // add on the secondVal to the list of current connections
+			map.put(secondVal,map.get(secondVal)+firstVal); // add on the firstVal to the list of current connections
+		}
+		shortest=map.size();
 	}
 
-	public boolean contains(String letter)
-	{
-		return true;
+	public boolean contains(String letter){
+		return map.get(letter)!=null;
 	}
 
-	public void check(String first, String second, String placesUsed, int steps)
-	{
+	public void check(String first, String second, String placesUsed, int steps){
+		if(map.get(first).contains(second)){ // if we found a connection
+			found=true;
+			if(steps<shortest)
+				shortest=steps;
+		}
+		String currentConnections=map.get(first);
+		for(String connection : currentConnections.split(""))
+			if(!placesUsed.contains(connection)) // if we haven't checked this connection yet
+				check(connection,second,placesUsed+connection,steps+1); // check it and add it as a checked position
 	}
 
-	public String toString()
-	{
-		return "";
+	public String toString(){
+		return found ? "yes in "+shortest+" steps" : "no";
 	}
 }
