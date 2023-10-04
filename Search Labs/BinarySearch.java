ff878f4da25997e7be53ee850f10ceec2e40dfa3
Brandon A <brandonwalex@gmail.com>
2023-10-03 21:17:35 -0400

    Updates

--- Search Labs/BinarySearch.java
+++ Search Labs/BinarySearch.java
@@ -1,5 +1,18 @@
 public class BinarySearch{
+    // returns the correct location of an item in a sorted array or the compliment of the spot it should be in
     public static int binarySearch(int[] arr,int val){
-        return -1;
+        int front=0;
+        int back=arr.length-1;
+        int mid=0;
+        while(front<=back){
+            mid=(front+back)/2;
+            if(arr[mid]==val)
+                return mid;
+            else if(arr[mid]>val)
+                back=mid-1;
+            else
+                front=mid+1;
+        }
+        return front>mid ? -(front+1) : -(mid+1);
     }
 }
