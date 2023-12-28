9afbe47ee4b0609db6d4ba91eb53125415006c57
Brandon A <brandonwalex@gmail.com>
2023-12-28 13:36:58 -0600

    Updates

--- Abstract Classes/Cell.java
+++ Abstract Classes/Cell.java
@@ -6,89 +6,65 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Font;
 
-public abstract class Cell implements Locatable
-{
+public abstract class Cell implements Locatable{
 	private int xPos;
 	private int yPos;
 	private int width;
 	private int height;
 
-	public Cell()
-	{
+	public Cell(){
 		setPos(5,5);
 		setWidth(5);
 		setHeight(5);
 	}
 
-	public Cell(int x, int y)
-	{
-
-
+	public Cell(int x,int y){
 
 	}
 
-	public Cell(int x, int y, int w, int h)
-	{
-
-
+	public Cell(int x,int y,int w,int h){
 
 	}
 
-	public void setPos(int x, int y)
-	{
-
-
+	public void setPos(int x, int y){
 
 	}
 	
-	public void setX( int x )
-	{
-
+	public void setX(int x){
 
 	}
 	
-	public void setY( int y )
-	{
-
+	public void setY(int y){
 
 	}
 
-	public void setWidth(int w)
-	{
-
+	public void setWidth(int w){
 
 	}
 	
-	public void setHeight(int h)
-	{
-
+	public void setHeight(int h){
 
 	}
 	
-	public int getX()
-	{
+	public int getX(){
 		return 0;
 	}
 	
-	public int getY()
-	{
+	public int getY(){
 		return 0;
 	}	
 
-	public int getWidth()
-	{
+	public int getWidth(){
 		return 0;
 	}	
 	
-	public int getHeight()
-	{
+	public int getHeight(){
 		return 0;
 	}
 	
 	public abstract void draw(Graphics window);
 	
-	public String toString()
-	{
+	public String toString(){
 		return getX() + " " + getY() + " " + getWidth() + " " + getHeight();
 	}
 }
