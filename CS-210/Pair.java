43b567cba17c589f9df494c3d373d3f98177fa4f
Brandon A <brandonwalex@gmail.com>
2024-02-21 16:56:03 -0500

    Updates

---
+++ CS-210/Pair.java
@@ -0,0 +1,19 @@
+
+public class Pair<A, B> {
+
+    A one;
+    B two;
+
+    public Pair(A a, B b) {
+        one = a;
+        two = b;
+    }
+
+    public A getOne() {
+        return one;
+    }
+
+    public B getTwo() {
+        return two;
+    }
+}
